e5a72a3eac73b571e8332558e175b823295eb045
gunho kim <112845834+gunhokim21@users.noreply.github.com>
2022-11-07 17:19:55 +0900

    Update PolicyHandler.java
    remove korean

--- order/src/main/java/localfooddelivery/infra/PolicyHandler.java
+++ order/src/main/java/localfooddelivery/infra/PolicyHandler.java
@@ -18,26 +18,10 @@ import localfooddelivery.domain.*;
 @Service
 @Transactional
 public class PolicyHandler{
-    @Autowired 주문Repository 주문Repository;
     @Autowired OrderRepository orderRepository;
     
     @StreamListener(KafkaProcessor.INPUT)
     public void whatever(@Payload String eventString){}
-
-    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='배달시작됨'")
-    public void whenever배달시작됨_주문상태변경(@Payload 배달시작됨 배달시작됨){
-
-        배달시작됨 event = 배달시작됨;
-        System.out.println("\n\n##### listener 주문상태변경 : " + 배달시작됨 + "\n\n");
-
-
-        
-
-        // Sample Logic //
-
-        
-
-    }
 
     @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='DeliveryStarted'")
     public void wheneverDeliveryStarted_OrderStatusChange(@Payload DeliveryStarted deliveryStarted){
@@ -45,13 +29,8 @@ public class PolicyHandler{
         DeliveryStarted event = deliveryStarted;
         System.out.println("\n\n##### listener OrderStatusChange : " + deliveryStarted + "\n\n");
 
-
-        
-
         // Sample Logic //
         Order.orderStatusChange(event);
-        
-
         
 
     }
