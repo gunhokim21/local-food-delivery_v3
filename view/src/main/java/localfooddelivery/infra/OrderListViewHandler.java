40d61710f7a3872cc30691959e9e33cc6978f665
gunho kim <112845834+gunhokim21@users.noreply.github.com>
2022-11-08 14:29:24 +0900

    Update OrderListViewHandler.java

--- view/src/main/java/localfooddelivery/infra/OrderListViewHandler.java
+++ view/src/main/java/localfooddelivery/infra/OrderListViewHandler.java
@@ -14,45 +14,51 @@ import java.util.Optional;
 @Service
 public class OrderListViewHandler {
 
-
     @Autowired
     private OrderListRepository orderListRepository;
 
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenOrderPlaced_then_CREATE_1 (@Payload OrderPlaced orderPlaced) {
+
+
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='PaymentApproved'")
+    public void whenPaymentApproved_then_CREATE_1(@Payload PaymentApproved paymentApproved) {
+        try {
+            if (!paymentApproved.validate()) return;
+
+                // view 객체 생성
+                OrderList orderList = new OrderList();
+                // view 객체에 이벤트의 Value 를 set 함
+                orderList.setOrderId(paymentApproved.getOrderId());
+                orderList.setPayStatus(paymentApproved.getStatus());
+
+                // view 레파지 토리에 save
+                orderListRepository.save(orderList);
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+    }
+
+
+
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='OrderPlaced'")
+    public void whenOrderPlaced_then_UPDATE_1 (@Payload OrderPlaced orderPlaced) {
         try {
 
             if (!orderPlaced.validate()) return;
 
-            // view 객체 생성
-            OrderList orderList = new OrderList();
-            // view 객체에 이벤트의 Value 를 set 함
-            orderList.setOrderId(orderPlaced.getOrderId());
-            orderList.setMenu(orderPlaced.getMenu());
-            orderList.setQty(orderPlaced.getQty());
-            orderList.setOrderStatus(orderPlaced.getStatus());
-            orderList.setUserId(orderPlaced.getUserId());
-            orderList.setAddress(orderPlaced.getAddress());
-            // view 레파지 토리에 save
-            orderListRepository.save(orderList);
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenPaymentApproved_then_UPDATE_1(@Payload PaymentApproved paymentApproved) {
-        try {
-            if (!paymentApproved.validate()) return;
-                // view 객체 조회
-            Optional<OrderList> orderListOptional = orderListRepository.findByOrderId(paymentApproved.getOrderId());
+            Optional<OrderList> orderListOptional = orderListRepository.findById(orderPlaced.getOrderId());
 
             if( orderListOptional.isPresent()) {
                  OrderList orderList = orderListOptional.get();
             // view 객체에 이벤트의 eventDirectValue 를 set 함
-                orderList.setPayStatus(paymentApproved.getStatus());    
+
+                orderList.setMenu(orderPlaced.getMenu());
+                orderList.setQty(orderPlaced.getQty());
+                orderList.setOrderStatus(orderPlaced.getStatus());
+                orderList.setUserId(orderPlaced.getUserId());
+                orderList.setAddress(orderPlaced.getAddress());
+
                 // view 레파지 토리에 save
                  orderListRepository.save(orderList);
                 }
@@ -62,17 +68,39 @@ public class OrderListViewHandler {
             e.printStackTrace();
         }
     }
-    @StreamListener(KafkaProcessor.INPUT)
+
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='PaymentCanceled'")
     public void whenPaymentCanceled_then_UPDATE_2(@Payload PaymentCanceled paymentCanceled) {
         try {
             if (!paymentCanceled.validate()) return;
                 // view 객체 조회
-            Optional<OrderList> orderListOptional = orderListRepository.findByOrderId(paymentCanceled.getOrderId());
+            Optional<OrderList> orderListOptional = orderListRepository.findById(paymentCanceled.getOrderId());
 
             if( orderListOptional.isPresent()) {
                  OrderList orderList = orderListOptional.get();
             // view 객체에 이벤트의 eventDirectValue 를 set 함
                 orderList.setPayStatus(paymentCanceled.getStatus());    
+                // view 레파지 토리에 save
+                 orderListRepository.save(orderList);
+                }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='OrderCanceled'")
+    public void whenOrderCanceled_then_UPDATE_3(@Payload OrderCanceled orderCanceled) {
+        try {
+            if (!orderCanceled.validate()) return;
+                // view 객체 조회
+            Optional<OrderList> orderListOptional = orderListRepository.findById(orderCanceled.getOrderId());
+
+            if( orderListOptional.isPresent()) {
+                 OrderList orderList = orderListOptional.get();
+            // view 객체에 이벤트의 eventDirectValue 를 set 함
+                orderList.setOrderStatus("취소됨");    
                 // view 레파지 토리에 save
                  orderListRepository.save(orderList);
                 }
