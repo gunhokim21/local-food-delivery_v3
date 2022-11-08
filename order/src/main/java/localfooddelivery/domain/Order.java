a2d056d6c422361b56f4bd1734aae91693fd6d31
gunho kim <112845834+gunhokim21@users.noreply.github.com>
2022-11-08 09:25:24 +0900

    add order process
    add order process

--- order/src/main/java/localfooddelivery/domain/Order.java
+++ order/src/main/java/localfooddelivery/domain/Order.java
@@ -18,41 +18,20 @@ public class Order  {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     
-    
-    
-    
-    
     private Long orderId;
-    
-    
-    
-    
     
     private String menu;
     
-    
-    
-    
-    
     private Integer qty;
-    
-    
-    
-    
     
     private String status;
     
-    
-    
-    
-    
     private String userId;
     
-    
-    
-    
-    
     private String address;
+
+    private Integer price;
+
 
     @PostPersist
     public void onPostPersist(){
@@ -63,6 +42,15 @@ public class Order  {
 
         localfooddelivery.external.Payment payment = new localfooddelivery.external.Payment();
         // mappings goes here
+
+        if (price.equals(null)) {
+            price = 0;
+        }
+        
+        payment.setOrderId(orderId);
+        payment.setAmount(price * qty);
+        payment.setStatus(status);
+
         OrderApplication.applicationContext.getBean(localfooddelivery.external.PaymentService.class)
             .approvePayment(payment);
 
@@ -74,22 +62,22 @@ public class Order  {
     @PrePersist
     public void onPrePersist(){
 
+        this.status = "주문됨";
 
-        OrderCanceled orderCanceled = new OrderCanceled(this);
-        orderCanceled.publishAfterCommit();
+        // OrderCanceled orderCanceled = new OrderCanceled(this);
+        // orderCanceled.publishAfterCommit();
 
     }
     @PreRemove
     public void onPreRemove(){
+        OrderCanceled orderCancelled = new OrderCanceled(this);
+        orderCancelled.publishAfterCommit();
     }
 
     public static OrderRepository repository(){
         OrderRepository orderRepository = OrderApplication.applicationContext.getBean(OrderRepository.class);
         return orderRepository;
     }
-
-
-
 
     public static void orderStatusChange(DeliveryStarted deliveryStarted){
 
@@ -110,8 +98,22 @@ public class Order  {
          });
         */
 
-        
+        repository().findById(deliveryStarted.getOrderId()).ifPresent(order->{
+            
+            order.setStatus(deliveryStarted.getStatus());
+            repository().save(order);
+
+         });
     }
 
+    @PostLoad
+    public void makeDelay(){
+        // For Circuit Breaker Test
+        // try {
+        //     Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+    }
 
 }
