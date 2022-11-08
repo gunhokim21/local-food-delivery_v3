97f88f7b8c4bd7ff7f50c3e092592fa4eab46916
gunho kim <112845834+gunhokim21@users.noreply.github.com>
2022-11-08 09:29:10 +0900

    Update Store.java

--- store/src/main/java/localfooddelivery/domain/Store.java
+++ store/src/main/java/localfooddelivery/domain/Store.java
@@ -1,8 +1,15 @@
 package localfooddelivery.domain;
 
 import localfooddelivery.domain.DeliveryStarted;
+import localfooddelivery.external.Order;
+import localfooddelivery.external.OrderService;
 import localfooddelivery.StoreApplication;
 import javax.persistence.*;
+
+import com.fasterxml.jackson.databind.Module.SetupContext;
+
+import javafx.application.Application;
+
 import java.util.List;
 import lombok.Data;
 import java.util.Date;
@@ -17,56 +24,31 @@ public class Store  {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     
-    
-    
-    
-    
     private Long storeOrderId;
-    
-    
-    
-    
     
     private Long orderId;
     
-    
-    
-    
-    
     private String userid;
-    
-    
-    
-    
     
     private String menu;
     
-    
-    
-    
-    
     private String address;
     
-    
-    
-    
-    
-    private String qty;
-    
-    
-    
-    
+    private Integer qty;
     
     private String status;
 
     @PostPersist
     public void onPostPersist(){
 
-
-        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
-        deliveryStarted.publishAfterCommit();
-
     }
+
+    @PrePersist
+    public void onPrePersist() {
+        // Get request from Inventory
+    }
+
+
     @PreUpdate
     public void onPreUpdate(){
         // Get request from Order
@@ -74,14 +56,46 @@ public class Store  {
         //    Application.applicationContext.getBean(localfooddelivery.external.OrderService.class)
         //    .getOrder(/** mapping value needed */);
 
+  
+        if (status.equals("배달시작")) {
+            setStatus("배달시작됨");
+  
+        } else if ( status.equals("취소됨") ) {
+            
+        
+        } else {
+            Order order =
+                StoreApplication.applicationContext.getBean(OrderService.class)
+                .getOrder(getStoreOrderId());
+
+            if(order.getStatus().equals("주문됨")) {
+                setUserid(order.getUserId());
+                setMenu(order.getMenu());
+                setAddress(order.getAddress());
+                setQty(order.getQty());
+                setOrderId(getStoreOrderId());
+                setStatus("조리시작됨");
+                // For Circuit Breaker 
+                // setStatus("주문됨");
+            } else if (order.getStatus().equals("주문상태조회오류")) {
+                // for fillback 
+                setStatus("주문상태조회오류");
+            } else 
+            throw new RuntimeException("Invalid Order!");
+        }
+
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
+        deliveryStarted.publishAfterCommit();
     }
 
     public static StoreRepository repository(){
         StoreRepository storeRepository = StoreApplication.applicationContext.getBean(StoreRepository.class);
         return storeRepository;
     }
-
-
 
 
     public static void orderReceive(PaymentApproved paymentApproved){
@@ -102,7 +116,12 @@ public class Store  {
 
          });
         */
+        Store store = new Store();
 
+        store.setOrderId(paymentApproved.getOrderId());
+        store.setStatus(paymentApproved.getStatus());
+
+        repository().save(store);
         
     }
     public static void orderCancel(PaymentCanceled paymentCanceled){
@@ -124,7 +143,13 @@ public class Store  {
          });
         */
 
-        
+        // modified 
+        repository().findById(paymentCanceled.getOrderId()).ifPresent(store->{
+            store.setStatus("취소됨");                
+            repository().save(store);        
+
+//            repository().delete(order);
+        });    
     }
 
 
